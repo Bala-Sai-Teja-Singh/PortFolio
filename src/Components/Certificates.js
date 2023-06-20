67652abfc809e2d1fb072cb69f8723394ea29e56
Bala Sai Teja Singh <balasaitejasingh@Balas-MacBook-Air.local>
2023-06-20 19:18:44 +0530

    deployed

--- src/Components/Certificates.js
+++ src/Components/Certificates.js
@@ -18,7 +18,7 @@ export default function Certificates() {
       backgroundSize: "cover",
     }}
   >
-    <div id="main-c"  className={`App ${imagesLoaded ? "visible" : "hidden"}`}>
+    <div id="main-c" >
       <div className="title">
         <h1>My Certifications</h1>
       </div>
@@ -27,15 +27,16 @@ export default function Certificates() {
           className="Nav-link-left size"
           to="/Certificates/WebDevelopment"
         >
-          <div id="c1">
+          <div id="c1" className={`App ${imagesLoaded ? "visible" : "hidden"}`}>
             <div style={{height:"100%", width:"100%"}}>
-            <img src={require("../Images/web4.jpg")} alt="WebImage" />
+            <img src={require("../Images/web4.jpg")} alt="WebImage" onLoad={handleImageLoad}/>
             </div>
           </div>
         </NavLink>
         <NavLink className="Nav-link-right size" to="/Certificates/Cloud" >
-          <div id="c2">
-            <img loading="lazy" 
+          <div id="c2" className={`App ${imagesLoaded ? "visible" : "hidden"}`}>
+            <img
+            onLoad={handleImageLoad}
               src={require("../Images/aws_logo.png")}
               alt="facebookicon"
             />
@@ -45,8 +46,9 @@ export default function Certificates() {
           className="Nav-link-left size"
           to="/Certificates/ProgrammingLanguages"
         >
-          <div id="c3">
-            <img loading="lazy" 
+          <div id="c3" className={`App ${imagesLoaded ? "visible" : "hidden"}`}>
+            <img
+            onLoad={handleImageLoad}
               src={require("../Images/Programming-languages.jpeg")}
               alt="facebookicon"
             />
@@ -56,8 +58,9 @@ export default function Certificates() {
           className="Nav-link-right size"
           to="/Certificates/Databases"
         >
-          <div id="c4">
-            <img loading="lazy" 
+          <div id="c4" className={`App ${imagesLoaded ? "visible" : "hidden"}`}>
+            <img
+            onLoad={handleImageLoad}
               src={require("../Images/database-services.jpg")}
               alt="facebookicon"
             />
@@ -67,11 +70,12 @@ export default function Certificates() {
           className="Nav-link-left size"
           to="/Certificates/Android-app-dev"
         >
-          <div id="c5">
-            <img loading="lazy" 
+          <div id="c5" className={`App ${imagesLoaded ? "visible" : "hidden"}`}>
+            <img 
+            onLoad={handleImageLoad}
               src={require("../Images/android.png")}
               alt="facebookicon"
-              onLoad={handleImageLoad}
+              
             />
           </div>
         </NavLink>
